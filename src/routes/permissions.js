fab16463f55c936f5f2084af2884ee58e451a448
monjane10 <louramonja52.n@gmail.com>
2025-08-13 12:39:32 +0200

    Adicionando metodo para criar permissao e ver detalhes da permissao

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -1,31 +1,45 @@
 import { Router } from 'express';
 import Permission from '../models/Permission.js';
+import User from '../models/User.js';
+import Document from '../models/Document.js';
 
 const permissionsRouter = Router();
 
 
 // Definir permissão de utilizador num documento
 permissionsRouter.post('/', async (req, res) => {
-  try {
-    const { userId, documentId, access } = req.body;
-    if (!userId || !documentId || !access) {
-      return res.status(400).json({ error: 'Campos obrigatórios em falta.' });
+    try {
+        const { userId, documentId, access } = req.body;
+        if (!userId || !documentId || !access) {
+            return res.status(400).json({ error: 'Campos obrigatórios em falta.' });
+        }
+        const permission = await Permission.create({ userId, documentId, access });
+        res.status(201).json(permission);
+    } catch (err) {
+        res.status(500).json({ error: 'Erro ao definir permissão.', details: err.message });
     }
-    const permission = await Permission.create({ userId, documentId, access });
-    res.status(201).json(permission);
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao definir permissão.', details: err.message });
-  }
 });
 
 // Listar permissões de um documento
 permissionsRouter.get('/:documentId', async (req, res) => {
-  try {
-    const permissions = await Permission.findAll({ where: { documentId: req.params.documentId } });
-    res.json(permissions);
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao listar permissões.' });
-  }
-});
+    try {
+        const permissions = await Permission.findAll({
+            where: { documentId: req.params.documentId },
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'username', 'email'] // pega o nome e o email do colaborador
+                },
+                {
+                    model: Document,
+                    attributes: ['id', 'title', 'content'] // pega o título e o conteudo do documento
+                }
+            ]
+        });
 
+        res.json(permissions);
+    } catch (err) {
+        res.status(500).json({ error: 'Erro ao listar permissões.' });
+    }
+});
 export default permissionsRouter;
